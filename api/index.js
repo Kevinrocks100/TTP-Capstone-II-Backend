e97e7320a1b9cae05540dea08d8738219ef780e0
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-08-17 19:07:08 -0400

    changed test.js to repo.js in api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-router.use("/test", require("./test"));
+router.use("/repo", require("./repo"));
 router.use((req, res, next) => {
     // const error = new Error("404 Not Found");
     // error.status = 404;
---
+++ api/repo.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/:username", async (req, res, next) => {
+    const githubUser = req.params.username;
+    try {
+        const allRepos = await axios.get(`https://api.github.com/users/${githubUser}/repos`);
+        allRepos
+            ? res.status(200).json(allRepos.data)
+            : res.status(404).send("not found");
+    }catch (error){
+        next(error);
+    }
+});
+
+router.get("/:owner/:repo", async (req, res, next) => {
+    const {owner, repo} = req.params;
+    try{
+        const repo = await axios.get(`https://api.github.com/repos/${owner}/${repo}`)
+    } catch (error) {
+        next(error);
+    }
+});
+module.exports = router;
