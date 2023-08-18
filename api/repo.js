b691c0a818ea6d606f6e35394536b77b163566e8
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-08-17 23:21:42 -0400

    set up get for single repo

--- api/repo.js
+++ api/repo.js
@@ -17,9 +17,14 @@ router.get("/:username", async (req, res, next) => {
 router.get("/:owner/:repo", async (req, res, next) => {
     const {owner, repo} = req.params;
     try{
-        const repo = await axios.get(`https://api.github.com/repos/${owner}/${repo}`)
+        const repo = await axios.get(`https://api.github.com/repos/${owner}/${repo}`);
+        repo
+            ? res.status(200).json(repo.data)
+            : res.status(404).send("not found");
     } catch (error) {
         next(error);
     }
 });
+
+
 module.exports = router;
