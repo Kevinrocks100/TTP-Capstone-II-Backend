cc79008092f2061c5aeaa9dd3282ab337102eebb
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-08-18 00:45:33 -0400

    api set up to request list all pull requests get specific pull request

--- api/repo.js
+++ api/repo.js
@@ -14,16 +14,42 @@ router.get("/:username", async (req, res, next) => {
     }
 });
 
-router.get("/:owner/:repo", async (req, res, next) => {
-    const {owner, repo} = req.params;
+router.get("/:owner/:reqrepo", async (req, res, next) => {
+    const {owner, reqrepo} = req.params;
     try{
-        const repo = await axios.get(`https://api.github.com/repos/${owner}/${repo}`);
+        const repo = await axios.get(`https://api.github.com/repos/${owner}/${reqrepo}`);
         repo
             ? res.status(200).json(repo.data)
             : res.status(404).send("not found");
     } catch (error) {
         next(error);
     }
+});
+
+router.get("/:owner/:reqrepo/pulls", async (req, res, next) => {
+    const { owner, reqrepo } = req.params;
+    try{
+        const pulls = await axios.get(`https://api.github.com/repos/${owner}/${reqrepo}/pulls?state=all`);
+        pulls
+            ? res.status(200).json(pulls.data)
+            : res.status(404).send("not found");
+    } catch (error) {
+        next(error);
+    }
+
+});
+
+router.get("/:owner/:reqrepo/pulls/:pullnumber", async (req, res, next) => {
+    const { owner, reqrepo, pullnumber } = req.params;
+    try{
+        const pull = await axios.get(`https://api.github.com/repos/${owner}/${reqrepo}/pulls?state=all/${pullnumber}`);
+        pull
+            ? res.status(200).json(pull.data)
+            : res.status(404).send("not found");
+    } catch (error) {
+        next(error);
+    }
+
 });
 
 
